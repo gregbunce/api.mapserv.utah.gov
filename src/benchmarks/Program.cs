d44c7d0092dde744197c2f68e3c990f8ed893c99
steveoh <sgourley@utah.gov>
2020-06-18 12:57:14 -0600

    feat(api): add benchmark.net

---
+++ src/benchmarks/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+using benchmarks.tests;
+
+namespace benchmarks
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args is null) {
+                throw new ArgumentNullException(nameof(args));
+            }
+
+            _ = BenchmarkRunner.Run<AddressParsingBenchMark>();
+        }
+    }
+}
---
+++ src/benchmarks/tests/AddressParsingBenchMark.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using api.mapserv.utah.gov.Models;
+using MediatR;
+using Moq;
+using api.mapserv.utah.gov.Features.Geocoding;
+using api.mapserv.utah.gov.Cache;
+using Serilog;
+using System.Threading;
+
+namespace benchmarks.tests {
+    [RPlotExporter, CategoriesColumn, RankColumn, MeanColumn, MedianColumn, MemoryDiagnoser]
+    public class AddressParsingBenchMark {
+        private readonly IRequestHandler<AddressParsing.Command, CleansedAddress> _handler;
+        private readonly AddressParsing.Command _po_box_request;
+        private readonly AddressParsing.Command _unit_with_no_type;
+
+        public AddressParsingBenchMark() {
+            var abbreviations = new Abbreviations();
+            var regex = new RegexCache(abbreviations);
+
+            _handler = new AddressParsing.Handler(regex, abbreviations, new Mock<ILogger>().Object);
+
+            _po_box_request = new AddressParsing.Command("P O Box 123");
+            _unit_with_no_type = new AddressParsing.Command("625 NORTH REDWOOD ROAD 6");
+        }
+
+        [Benchmark]
+        public async Task<CleansedAddress> PoBoxes () => await _handler.Handle(_po_box_request, CancellationToken.None);
+
+        [Benchmark]
+        public async Task<CleansedAddress> UnitsWithNoTypes () => await _handler.Handle(_unit_with_no_type, CancellationToken.None);
+
+        }
+}
